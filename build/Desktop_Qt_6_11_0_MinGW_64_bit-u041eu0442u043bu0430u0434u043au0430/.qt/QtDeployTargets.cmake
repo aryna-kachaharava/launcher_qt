0d5447b1d13de26d42bf670f9a0ba3c71a004cac
aryna-kachaharava <arishka21.03.2008@gmail.com>
2026-04-23 07:51:11 +0300

    Delete build directory

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-u041eu0442u043bu0430u0434u043au0430/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_launcher_FILE D:/QT projects/launcher/build/Desktop_Qt_6_11_0_MinGW_64_bit-u041eu0442u043bu0430u0434u043au0430/launcher.exe)
-set(__QT_DEPLOY_TARGET_launcher_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_launcher_RUNTIME_DLLS D:/qt_downloads/6.11.0/mingw_64/bin/Qt6Widgets.dll;D:/qt_downloads/6.11.0/mingw_64/bin/Qt6Gui.dll;D:/qt_downloads/6.11.0/mingw_64/bin/Qt6Core.dll)
